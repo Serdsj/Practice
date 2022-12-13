4507fa837aa4ae86c1adca21327316c076841b9a
unknown <lifannnnof@mail.ru>
2022-12-13 23:11:09 +0300

    pervaia zadacha start

---
+++ src/Трансформировать в объекты.js
@@ -0,0 +1,10 @@
+'use strict'
+const vasya = { name: "Vasya", surname: "Pupkin", id: 1 };
+const petya = { name: "Petya", surname: "Ivanov", id: 2 };
+const masha = { name: "Masha", surname: "Berger", id: 3 };
+
+const users = [vasya, petya, masha];
+
+const usersMaped = users.map ( function (item) {
+    
+})
