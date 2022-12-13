aa16b0aa8dbcc1eeec37e7e4222fa0edc65136dd
unknown <lifannnnof@mail.ru>
2022-12-14 00:52:08 +0300

    чекни

--- src/Трансформировать в объекты.js
+++ src/Трансформировать в объекты.js
@@ -6,5 +6,12 @@ const masha = { name: "Masha", surname: "Berger", id: 3 };
 const users = [vasya, petya, masha];
 
 const usersMaped = users.map ( function (item) {
-    
-})
+   return item = [
+        { fullName: "Вася Пупкин", id: 1 },
+        { fullName: "Петя Иванов", id: 2 },
+        { fullName: "Маша Петрова", id: 3 }
+      ]
+});
+
+console.log (usersMaped[0].id)
+console.log (usersMaped[0].fullName)
