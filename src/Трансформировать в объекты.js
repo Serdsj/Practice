d268ec3cf886101ef5f0c7b011ad6a4cf6b00109
unknown <lifannnnof@mail.ru>
2022-12-15 23:39:47 +0300

    vtoroy variant

--- src/Трансформировать в объекты.js
+++ src/Трансформировать в объекты.js
@@ -5,13 +5,19 @@ const masha = { name: "Masha", surname: "Berger", id: 3 };
 
 const users = [vasya, petya, masha];
 
-const usersMaped = users.map ( function (item) {
-   return item = [
-        { fullName: "Вася Пупкин", id: 1 },
-        { fullName: "Петя Иванов", id: 2 },
-        { fullName: "Маша Петрова", id: 3 }
-      ]
+let usersMapped = users.map(function (item) {
+  const name = name.item;
+  const surname = surname.item;
+  const id = id.item;
+  const fullName = fullName.item;
+  const massiv = [];
+  for (let i = 0; i < massiv.length; i++) {
+    const save = [i];
+    if (save === name && save === surname) {
+      return (massiv = [(fullName = ` ${name} ${surname} `), id]);
+    }
+  }
 });
 
-console.log (usersMaped[0].id)
-console.log (usersMaped[0].fullName)
+console.log(usersMapped[0].fullName);
+console.log(usersMapped[0].id);
